c26cb5d44e05c8bddd764abd54bf3ec2ce7faac7
antonKalinin <hurricanebox@gmail.com>
2013-04-23 17:37:26 +0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,24 @@ exports.home = function(req, res) {
 
 exports.uploadHandler = function(req, res) {
     console.log('Upload started...');
-    console.log(req);
+    var image = req.files.image
+    
+    if (!image) {
+        res.send({res: false});
+        return false;    
+    }
+    
+    var vs = require('fs');
+    fs.readFile(req.files.displayImage.path, function (err, data) {
+        // asynchronously reads the entire contents of an image file
+        var newPath = __dirname + "/uploads/" + new Date().getTime();
+        fs.writeFile(newPath, data, function (err) {
+            var ajaxResp = {
+                res: true,
+                imagePath: newPath
+            }
+            res.send(ajaxResp);
+        });
+    });
     res.send('ok');
 };
