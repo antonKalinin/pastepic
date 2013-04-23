392c754dd80b73236ef5332ac3943a9fa57351de
antonKalinin <hurricanebox@gmail.com>
2013-04-23 14:54:01 +0400

    test upload pasted image

--- public/js/common.js
+++ public/js/common.js
@@ -3,21 +3,44 @@ $(function() {
     function pasteHandler(event){
         var items = event.clipboardData.items;
 
-        /* Detect if last item is image */
-        var type = items[0].type;
+        /* Get only first item of clipboard (last inserted) */
+        var item = items[0];
+        var type = item.type;
+        
+        /* Detect if item is image */
         var isImage = (type.match(/^image/ig)).length > 0;
-        if (isImage) {
-            var blob = items[0].getAsFile();
-            var reader = new FileReader();
-            reader.onload = function(event){
-                var imgSrc = event.target.result;
-                console.log(imgSrc);
-                var $img = $('<img />').attr('src', imgSrc);
-                $('.image-holder .content-tip').hide();
-                $('.image-holder').append($img);
-            };
-            reader.readAsDataURL(blob);
+        if (!isImage) {
+            return false;
         }
+        
+        var formData = new FormData(),
+            reader = new FileReader(),
+            blob = item.getAsFile();
+            
+        reader.onload = function(event){
+            var imgSrc = event.target.result;
+            console.log(imgSrc);
+            var $img = $('<img />').attr('src', imgSrc);
+            $('.image-holder .content-tip').hide();
+            $('.image-holder').append($img);
+        };
+
+        reader.readAsDataURL(blob);
+        
+        /* try to upload image to server */
+        formData.append('image', blob);
+        $.ajax({
+            type: "POST",
+            url: '/upload',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log('data');    
+            },
+            dataType: 'json'
+        });
     };
     
     document.onpaste = pasteHandler;
