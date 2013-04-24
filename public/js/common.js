7c21f8754ddc1246e6c5939ba2f706c73ef77f0d
Anton <antonkalinin@Antons-MacBook-Pro.local>
2013-04-24 09:08:53 +0400

    Enable image saving

--- public/js/common.js
+++ public/js/common.js
@@ -2,16 +2,20 @@ $(function() {
     /* Bind paste event */
     function pasteHandler(event){
         var items = event.clipboardData.items;
-
-        /* Get only first item of clipboard (last inserted) */
-        var item = items[0];
-        var type = item.type;
-        
-        /* Detect if item is image */
-        var isImage = (type.match(/^image/ig)).length > 0;
-        if (!isImage) {
+        if (!items.length) {
+            console.log("Nothing to paste.");
             return false;
         }
+
+        /* Get only first item of clipboard (last inserted) */
+        var item = items[0],
+            type = item.type;
+
+        /* Detect if item is image */
+        if (!type.match(/^image/ig)) {
+            return false;
+        }
+
         
         var formData = new FormData(),
             reader = new FileReader(),
@@ -19,8 +23,12 @@ $(function() {
             
         reader.onload = function(event){
             var imgSrc = event.target.result;
-            console.log(imgSrc);
-            var $img = $('<img />').attr('src', imgSrc);
+
+            var $img = $('.image-holder img');
+            if(!$img.length) {
+                $img = $('<img />')
+            }
+            $img.attr('src', imgSrc);
             $('.image-holder .content-tip').hide();
             $('.image-holder').append($img);
         };
@@ -37,7 +45,9 @@ $(function() {
             contentType: false,
             processData: false,
             success: function (data) {
-                console.log(data);    
+                if(data && data.imgId) {
+                    window.location = '/' + data.imgId;
+                }
             },
             dataType: 'json'
         });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 exports.home = function(req, res) {
-    res.render('home', {title: 'Pic.Talk'})
+    res.render('home', {title: 'PicTalk', imageSrc: false})
 };
 
 exports.uploadHandler = function(req, res) {
-    console.log('Upload started...');
+
     var image = req.files.image
     
     if (!image) {
@@ -11,17 +11,26 @@ exports.uploadHandler = function(req, res) {
         return false;    
     }
     
-    var vs = require('fs');
+    var fs = require('fs'),
+        path = require('path');
+
     fs.readFile(image.path, function (err, data) {
         // asynchronously reads the entire contents of an image file
-        var newPath = __dirname + "/uploads/" + new Date().getTime();
-        fs.writeFile(newPath, data, function (err) {
-            var ajaxResp = {
-                res: true,
-                imagePath: newPath
-            }
-            res.send(ajaxResp);
+        var uploadDir = path.join(__dirname, '../', '/public/uploads/');
+        var imgId = new Date().getTime();
+        var savePath = uploadDir + imgId + '.png';
+        fs.writeFile(savePath, data, function (err) {
+            if (err) throw err;
+            res.send({imgId: imgId});
         });
     });
-    res.send('ok');
+};
+
+exports.imageHandler = function(req, res) {
+    console.log(req);
+    var viewData = {
+        title: 'PicTalk',
+        imageSrc: '/uploads/' + req.route.params.imageId + '.png'
+    }
+    res.render('home', viewData)
 };
