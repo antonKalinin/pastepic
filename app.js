6d9d3cf2f1a43039695c0f46c5023a14ab036197
antonKalinin <hurricanebox@gmail.com>
2013-04-29 13:56:07 +0400

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ io.sockets.on('connection', function (socket) {
          * Info other sockets in the room about new picture viewer.
          * Count the number of online viewers.
          */
-         var picId = data.picId ? dataPicId : false;
+         var picId = data.picId ? data.PicId : false;
          if(!picId) return;
          /* 
           * Check if client already in room.
