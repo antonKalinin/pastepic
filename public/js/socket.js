b6dd528ef9cba09008ab5d40c3cf08885dca4b21
antonKalinin <hurricanebox@gmail.com>
2013-04-29 13:54:49 +0400

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,26 +1,22 @@
 window.onload = function() {
-    // Create a connection to server. In chrome use long polling somewhy! (TODO: get know why)
-    /* if (navigator.userAgent.toLowerCase().indexOf('chrome') != -1) {
-        socket = io.connect('http://fronteeth.com:8080', {'transports': ['xhr-polling']});
-    } else {
-        
-    } */
     
-    // socket = io.connect('http://fronteeth.com:8080');
-    // socket.on('connect', function () {
+    // Create a connection to server.
+    socket = io.connect('http://fronteeth.com:8080');
+    socket.on('connect', function (data) {
+        console.log(data);
         
         /* init connetion to picture page */
-        // socket.emit('picConnInit', { picId: app.getPicId() });
+        socket.emit('picConnInit', { picId: app.getPicId() });
         
         /* handle connection to picture page response from server */
-        // socket.on('picConnReps', function(data) {
-            //  console.log(data);
-        // });
+        socket.on('picConnReps', function(data) {
+            console.log(data);
+        });
 
-        // socket.on('message', function (data) {
-            // console.log(data);
-        // });
-    // });
+        socket.on('message', function (data) {
+            console.log(data);
+        });
+    });
 };
 
 function logEvent(event, data) {
