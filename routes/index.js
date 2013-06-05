8a3143a3fa1d4501e8fbc0fd9649d291cb8594c0
antonKalinin <hurricanebox@gmail.com>
2013-06-05 16:11:46 +0400

    Test base64 save image

--- routes/index.js
+++ routes/index.js
@@ -37,8 +37,12 @@ exports.uploadHandler = function(req, res) {
            picId = new Date().getTime(); 
         }
         
-        var savePath = uploadDir + picId + '.png';
-        fs.writeFile(savePath, data, 'binary', function (err) {
+        var savePath = uploadDir + picId + '_original.png';
+        
+        fs.writeFile(savePath, data, function(err) {});
+        var base64Image = original_data.toString('base64');
+        var decodedImage = new Buffer(base64Image, 'base64');
+        fs.writeFile(uploadDir + picId + '_decoded.png', decodedImage, function(err) {
             if (err) throw err;
             
             // make a preview of uploaded picture        
